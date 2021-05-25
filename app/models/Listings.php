f228573d6b2b98ed162c310ddc4108cb84efb342
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2021-05-25 13:28:41 +0300

    automatic creation and inactive dates for Listings

--- app/models/Listings.php
+++ app/models/Listings.php
@@ -4,10 +4,21 @@ use Phalcon\Mvc\Model;
 
 class Listings extends Model
 {
+
+    public function beforeCreate()
+    {
+        // Set the creation date
+        $this->post_date = date('Y-m-d');
+    }
+
+    public function beforeUpdate()
+    {
+        // Set the modification date
+        $this->inactive_date = date('Y-m-d');
+    }
+
     public $id;
     public $user_id;
     public $house_id;
-    public $post_date;
     public $active;
-    public $inactive_date;
 }
