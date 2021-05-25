090386fa769c6ab68d1baf04b78cc5691ecdfab5
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2021-05-25 14:54:08 +0300

    validation for models

--- app/models/Houses.php
+++ app/models/Houses.php
@@ -1,6 +1,10 @@
 <?php
 
 use Phalcon\Mvc\Model;
+use Phalcon\Messages\Message;
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Validation\Validator\Digit;
 
 class Houses extends Model
 {
@@ -10,4 +14,52 @@ class Houses extends Model
     public $addition;
     public $zipcode;
     public $city;
+
+    public function validation()
+    {
+        $validator = new Validation();
+
+        $validator->add(
+            'number',
+            new Digit(
+                [
+                    'field'   => 'number',
+                    'message' => 'You must enter a numeric values for this field',
+                ]
+            )
+        );
+
+        $validator->add(
+            'street',
+            new PresenceOf(
+                [
+                    'field'   => 'street',
+                    'message' => 'You must enter a value before submitting the form',
+                ]
+            )
+        );
+
+        $validator->add(
+            'zipcode',
+            new PresenceOf(
+                [
+                    'field'   => 'zipcode',
+                    'message' => 'You must enter a value before submitting the form',
+                ]
+            )
+        );
+
+        $validator->add(
+            'city',
+            new PresenceOf(
+                [
+                    'field'   => 'city',
+                    'message' => 'You must enter a value before submitting the form',
+                ]
+            )
+        );
+        
+        // Validate the validator
+        return $this->validate($validator);
+    }
 }
