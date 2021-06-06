78b9b20318e93cba1ede409836a7e4effc8728c6
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2021-06-06 17:01:45 +0300

    added template routing

--- app/config/router.php
+++ app/config/router.php
@@ -4,4 +4,75 @@ $router = $di->getRouter();
 
 // Define your routes here
 
+// House Routes
+$router->addGet(
+    '/houses',
+    'Houses::getHousesAllResponse'
+);
+
+$router->addPost(
+    '/houses',
+    'Houses::test'
+);
+
+$router->addPut(
+    '/houses',
+);
+
+$router->addDelete(
+    '/houses',
+);
+
+// Listings Routes
+$router->addGet(
+    '/listings',
+);
+
+$router->addPost(
+    '/listings',
+);
+
+$router->addPut(
+    '/listings',
+);
+
+$router->addDelete(
+    '/listings',
+);
+
+// Rooms Routes
+$router->addGet(
+    '/rooms',
+);
+
+$router->addPost(
+    '/rooms',
+);
+
+$router->addPut(
+    '/rooms',
+);
+
+$router->addDelete(
+    '/rooms',
+);
+
+
+// Users Routes
+$router->addGet(
+    '/users',
+);
+
+$router->addPost(
+    '/users',
+);
+
+$router->addPut(
+    '/users',
+);
+
+$router->addDelete(
+    '/users',
+);
+
 $router->handle($_SERVER['REQUEST_URI']);
